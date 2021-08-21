ed8b6382d7918891bdaaa81aad6afe2dd34d7e8c
Amanda <amandavieira321@gmail.com>
2021-08-21 17:49:24 -0300

    Configurado a autenticação da api

--- src/main/java/br/com/erudio/Startup.java
+++ src/main/java/br/com/erudio/Startup.java
@@ -12,6 +12,10 @@ public class Startup {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
+		
+		// Forma de gerar senha criptografada, para utilizar na api
+		// BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(16);
+		// String result = bCryptPasswordEncoder.encode("admin123");
 	}
 
 }
